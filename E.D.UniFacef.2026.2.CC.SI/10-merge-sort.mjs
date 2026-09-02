95b7256f0191dd2f59a4dc31cd6758f0b873f625
XandyGomes <x3.computadores@hotmail.com>
2026-09-02 19:28:13 -0300

    Implement merge sort for objects with comparison function and tracking of comparisons, divisions, and merges

--- E.D.UniFacef.2026.2.CC.SI/10-merge-sort.mjs
+++ E.D.UniFacef.2026.2.CC.SI/10-merge-sort.mjs
@@ -1,43 +1,50 @@
-function mergeSort(vetor) {
-  if (vetor.length < 2) return vetor;
+  function mergeSort(vetor) {
+    //condição de saída
+    if (vetor.length < 2) return vetor;
 
-  let meio = Math.floor(vetor.length / 2);
+    let meio = Math.floor(vetor.length / 2);
 
-  let vetEsq = vetor.slice(0, meio);
-  let vetDir = vetor.slice(meio);
+    let vetEsq = vetor.slice(0, meio);
+    let vetDir = vetor.slice(meio);
 
-  //chamadas recursivas ao mergeSort
-  vetEsq = mergeSort(vetEsq);
-  vetDir = mergeSort(vetDir);
+    //chamadas recursivas
+    vetEsq = mergeSort(vetEsq);
+    vetDir = mergeSort(vetDir);
 
-  //mesclagem ordenada de vetEsq com vetDir
-  let posEsq = 0,
-    posDir = 0,
-    vetRes = [];
+    //mesclagem ordenada de vetEsq com vetDir
 
-  while (2 < 2 && 2 < 3) {
-    if (vetEsq[1] < vetDir[2]) {
-      vetRes.push(vetEsq[posEsq]);
-      posEsq++;
-    } else {
-      vetRes.push(vetDir[posDir]);
-      posDir++;
+    let posEsq = 0,
+      posDir = 0,
+      vetRes = [];
+
+    while (posEsq < vetEsq.length && posDir < vetDir.length) {
+      if (vetEsq[posEsq] < vetDir[posDir]) {
+        vetRes.push(vetEsq[posEsq]);
+        posEsq++;
+      } else {
+        vetRes.push(vetDir[posDir]);
+        posDir++;
+      }
     }
+
+    let sobra;
+    if (posEsq < posDir) {
+      sobra = vetEsq.slice(posEsq);
+    } else {
+      sobra = vetDir.slice(posDir);
+    }
+
+    return [...vetRes, ...sobra];
   }
 
-  let sobra;
+// let nums = [77, 44, 22, 33, 99, 55, 88, 0, 66, 11];
 
-  if (posEsq < posDir) {
-    sobra = vetEsq.slice(posEsq);
-  } else {
-    sobra = vetDir.slice(posDir);
-  }
+// let numsOrd = mergeSort(nums)
 
-  return [...vetRes, ...sobra];
-}
+// console.log({numsOrd})
 
-let nums = [77, 44, 22, 33, 99, 55, 88, 0, 66, 11];
+import {nomes} from "./data/nomes-desord.mjs"
 
-let numsOrd = mergeSort(nums)
+let nomesOrd = mergeSort(nomes)
 
-console.log({numsOrd})
+console.log(nomesOrd)
--- E.D.UniFacef.2026.2.ES/10-merge-sort.mjs
+++ E.D.UniFacef.2026.2.ES/10-merge-sort.mjs
@@ -1,41 +1,41 @@
-function mergeSort(vetor) {
-  //condição de saída
-  if (vetor.length < 2) return vetor;
+  function mergeSort(vetor) {
+    //condição de saída
+    if (vetor.length < 2) return vetor;
 
-  let meio = Math.floor(vetor.length / 2);
+    let meio = Math.floor(vetor.length / 2);
 
-  let vetEsq = vetor.slice(0, meio);
-  let vetDir = vetor.slice(meio);
+    let vetEsq = vetor.slice(0, meio);
+    let vetDir = vetor.slice(meio);
 
-  //chamadas recursivas
-  vetEsq = mergeSort(vetEsq);
-  vetDir = mergeSort(vetDir);
+    //chamadas recursivas
+    vetEsq = mergeSort(vetEsq);
+    vetDir = mergeSort(vetDir);
 
-  //mesclagem ordenada de vetEsq com vetDir
+    //mesclagem ordenada de vetEsq com vetDir
 
-  let posEsq = 0,
-    posDir = 0,
-    vetRes = [];
+    let posEsq = 0,
+      posDir = 0,
+      vetRes = [];
 
-  while (posEsq < vetEsq.length && posDir < vetDir.length) {
-    if (vetEsq[posEsq] < vetDir[posDir]) {
-      vetRes.push(vetEsq[posEsq]);
-      posEsq++;
-    } else {
-      vetRes.push(vetDir[posDir]);
-      posDir++;
+    while (posEsq < vetEsq.length && posDir < vetDir.length) {
+      if (vetEsq[posEsq] < vetDir[posDir]) {
+        vetRes.push(vetEsq[posEsq]);
+        posEsq++;
+      } else {
+        vetRes.push(vetDir[posDir]);
+        posDir++;
+      }
     }
-  }
 
-  let sobra;
-  if (posEsq < posDir) {
-    sobra = vetEsq.slice(posEsq);
-  } else {
-    sobra = vetDir.slice(posDir);
-  }
+    let sobra;
+    if (posEsq < posDir) {
+      sobra = vetEsq.slice(posEsq);
+    } else {
+      sobra = vetDir.slice(posDir);
+    }
 
-  return [...vetRes, ...sobra];
-}
+    return [...vetRes, ...sobra];
+  }
 
 // let nums = [77, 44, 22, 33, 99, 55, 88, 0, 66, 11];
 
